4965eac77c14148c4e8fa364b0c1d2d237684c93
blueskylinks <blueskylinks@outlook.com>
2018-07-03 17:28:51 +0530

    changes made on login page

--- app/src/main/java/com/blueskylinks/spc_main/MainActivity.java
+++ app/src/main/java/com/blueskylinks/spc_main/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -16,24 +17,49 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
-    EditText et1;
-    EditText et2;
+
     public static String subId;
     String pass;
     TextView text;
     CheckBox c1;
-    SharedPreferences settings;
-    SharedPreferences sharedPreferences1;
+    SharedPreferences prefs;
+   EditText edt1;
     boolean value1 = true;
     public static String phoneNumber;
     TextView textView;
+    String Subno;
+    Spinner SPINNER;
+    Button ADD;
+    EditText EDITTEXT;
+    String[] spinnerItems = new String[]{
+         "9880760642"};
+    String GETTEXT;
+    String gettext="111";
+    List<String> stringlist;
+    ArrayAdapter<String> arrayadapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,97 +71,63 @@ public class MainActivity extends AppCompatActivity {
                 new String[]{Manifest.permission.READ_SMS}, 200);
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.RECEIVE_SMS}, 200);
-        et1 = findViewById(R.id.Et1);
-        et2 = findViewById(R.id.Et2);
-        text = findViewById(R.id.textView7);
-        Paint p = new Paint();
-        p.setColor(Color.BLUE);
-        c1 = findViewById(R.id.checkBox);
-        textView=findViewById(R.id.textView30);
-        textView.setPaintFlags(p.getColor());
-        textView.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
-
-        sharedPreferences1 = getSharedPreferences("isChecked1", 0);
 
 
-        value1 = sharedPreferences1.getBoolean("isChecked1", value1); // retrieve the value of your key
 
-        c1.setChecked(value1);
-        c1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    sharedPreferences1.edit().putBoolean("isChecked1", true).apply();
-                // update your model (or other business logic) based on isChecked
-              settings = getSharedPreferences("PREFS_NAME", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = settings.edit();
-                    subId = et1.getText().toString();
-                    pass = et2.getText().toString();
-                // Edit and commit
-                System.out.println("onPause save name: " + subId);
-                System.out.println("onPause save password: " + pass);
-                editor.putString("PREF_UNAME", subId);
-                editor.putString("PREF_PASSWORD", pass);
-                editor.commit();
+        SPINNER = (Spinner)findViewById(R.id.spinner1);
+        ADD = (Button)findViewById(R.id.button1);
+        EDITTEXT = (EditText)findViewById(R.id.editText1);
+        stringlist = new ArrayList<>(Arrays.asList(spinnerItems));
 
-                }
-                else {   sharedPreferences1.edit().putBoolean("isChecked1", false).apply();}
+        arrayadapter = new ArrayAdapter<String>(MainActivity.this,R.layout.textview,stringlist);
+
+        arrayadapter.setDropDownViewResource(R.layout.textview);
+        SPINNER.setAdapter(arrayadapter);
+
+        prefs = getSharedPreferences("TAG", Context.MODE_PRIVATE);
+        gettext= prefs.getString("KEY", null);
+        Log.i("value",gettext);
+        stringlist.add(gettext);
+        arrayadapter.notifyDataSetChanged();
+
+
+        SPINNER.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Subno = SPINNER.getItemAtPosition(i).toString();
+                Log.i("Selected item",Subno);
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
         });
+    }
 
-        et1.addTextChangedListener(new TextWatcher() {
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.length()==10){text.setText(" ");}
-                //Your query to fetch Data
-            }
+public void display_editBox(View v){
+    EDITTEXT.setVisibility(View.VISIBLE);
+    ADD.setVisibility(View.VISIBLE);
+}
 
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                    //Your query to fetch Data
-
-            }
-        });
-        et2.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Log.i("test",s.toString());
-                if(s.toString().equals("1234")){text.setText(" ");}
-                //Your query to fetch Data
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                //Your query to fetch Data
-            }
-        });
-
-        if(c1.isChecked()){
-            settings = getSharedPreferences("PREFS_NAME",Context.MODE_PRIVATE);
-            // Get value
-            subId = settings.getString("PREF_UNAME", " ");
-            pass = settings.getString("PREF_PASSWORD", " ");
-            et1.setText(subId);
-            et2.setText(pass);
-            System.out.println("onResume load name: " + subId);
-            System.out.println("onResume load password: " + pass);
-        }
-            else return;
-
+    public void update_spinner(View v){
+        GETTEXT = EDITTEXT.getText().toString();
+        if(stringlist.contains(GETTEXT)){}
+        else{
+        stringlist.add(GETTEXT);
+        arrayadapter.notifyDataSetChanged();
+        prefs = this.getSharedPreferences("TAG", Context.MODE_PRIVATE);
+        prefs.edit().putString("KEY", GETTEXT).commit();
+        Log.i("updated string",stringlist.toString());}
+        EDITTEXT.setVisibility(View.INVISIBLE);
+        ADD.setVisibility(View.INVISIBLE);
     }
 
     public void set_activity(View view) {
-        subId = et1.getText().toString();
+        /*subId = et1.getText().toString();
         pass = et2.getText().toString();
         Log.i("sub ID:",subId);
         Log.i("password",pass);
@@ -147,11 +139,12 @@ public class MainActivity extends AppCompatActivity {
         } else if (!pass.equals("1234")) {
             text.setText("Incorrect  password!!..");
         } else {
-            phoneNumber=subId;
+            phoneNumber=subId;*/
             //starting another activity..
+            subId=Subno;
             Intent it = new Intent(MainActivity.this, splashActivity.class);
             startActivity(it);
-        }
+
     }
 }
 
